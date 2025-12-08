655e683e7850c9a97e726b082cccd4538ec0081c
trapperdot00 <csernusricsike@gmail.com>
2025-12-08 19:23:15 +0100

    Removed unused chest-content file-writing function.

--- chest_parser.lua
+++ chest_parser.lua
@@ -1,6 +1,6 @@
 local chest_parser = {}
 
-function chest_parser.load_chest_contents()
+function chest_parser.get_chest_contents()
 	local chests = { peripheral.find("minecraft:chest") }
 	local contents = {}
 	for _, chest in ipairs(chests) do
@@ -8,11 +8,6 @@ function chest_parser.load_chest_contents()
 		contents[chest_name] = chest.list()
 	end
 	return contents
-end
-
-function chest_parser.save_chest_contents_to_file(contents, filename)
-	local serialized = textutils.serialize(contents)
-	print(serialized)
 end
 
 return chest_parser
