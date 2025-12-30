5998bd167b9283000fe26435f9a2605d71bc44cb
Csernus Richárd Benjámin <csernusricsike@gmail.com>
2025-12-30 19:40:41 +0100

    Rewritten pull to use the new iterator wrapper to abstract traversal.

--- src/cmd/pull.lua
+++ src/cmd/pull.lua
@@ -4,28 +4,30 @@ local pull = {}
 function pull.get_plans(self)
     self:load()
     local plans = {}
-    local src_it = self:get_output_iterator()
-    local dst_it = self:get_input_iterator()
+    local empty = function(it)
+        return it:get().item == nil
+    end
+    local occup = function(it)
+        return not empty(it)
+    end
+    local src_it = self:get_output_iterator(occup)
+    local dst_it = self:get_input_iterator(empty)
+    src_it:first()
+    dst_it:first()
     while not src_it:is_done() and
           not dst_it:is_done() do
         local src_data = src_it:get()
         local dst_data = dst_it:get()
-        if not src_data.item then
-            -- Input slot is empty
-            src_it:next()
-        elseif dst_data.item then
-            -- Output slot is occupied
-            dst_it:next()
-        else
-            local plan = {
-                src      = src_data.id,
-                dst      = dst_data.id,
-                src_slot = src_data.slot
-            }
-            table.insert(plans, plan)
-            src_it:next()
-            dst_it:next()
-        end
+        local plan = {
+            src      = src_data.id,
+            dst      = dst_data.id,
+            src_slot = src_data.slot,
+            dst_slot = dst_data.slot,
+            count    = src_data.item.count
+        }
+        table.insert(plans, plan)
+        src_it:next()
+        dst_it:next()
     end
     return plans
 end
