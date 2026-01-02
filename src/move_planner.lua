0d8578f70de131d88163d942027499ec3058b570
Csernus Richárd Benjámin <csernusricsike@gmail.com>
2026-01-02 07:39:54 +0100

    Updated move_planner to correctly top-up slots between inventories: now looks at stack sizes, and correctly handles multiple items inside the same inventory entities. Also added a generic empty-slot filler function that needs revision to top-up already existing slots when pushing non-full slots.

--- src/move_planner.lua
+++ src/move_planner.lua
@@ -1,32 +1,19 @@
+local fiter = require("src.filter_iterator")
 local move_planner = {}
 
-local function print_inv(inv)
-    local inv_ids = inv:get_inv_ids()
-    for _, inv_id in ipairs(inv_ids) do
-        local inv_size  = inv:get_size(inv_id)
-        local inv_items = inv:get_items(inv_id)
-        print(inv_id, inv_size)
-        for slot, item in pairs(inv_items) do
-            print(slot, item.name, item.count)
-        end
-    end
-end
-
-local function top_up(db, src_id, dst_id)
+-- Create plans for moving items
+-- into new slots.
+-- TODO: top-up previously moved nonfull slots!
+function move_planner.move
+(db, stacks, src_id, dst_id)
     local plans = {}
-    local stack = 64
-    local src_pred = function (it)
-        local curr = it:get()
-        local item = curr.item
-        return  item
-            and curr.id == src_id
+    local src_pred = function(curr)
+        return curr.item ~= nil
+             and curr.id == src_id
     end
-    local dst_pred = function (it)
-        local curr = it:get()
-        local item = curr.item
-        return  item
-            and curr.id == dst_id
-            and item.count < stack
+    local dst_pred = function(curr)
+        return curr.item == nil
+             and curr.id == dst_id
     end
     local src_it = fiter:new(db, src_pred)
     local dst_it = fiter:new(db, dst_pred)
@@ -36,48 +23,97 @@ local function top_up(db, src_id, dst_id)
           not dst_it:is_done() do
         local src_val = src_it:get()
         local dst_val = dst_it:get()
-        
-        local cap = stack - dst_val.item.count
-        local cnt = math.min(src_val.item.count, cap)
-        
-        local src_cnt = src_val.item.count - cnt
-        local dst_cnt = dst_val.item.count + cnt
-        
         local plan = {
-            src_id   = src_val.id,
+            src      = src_val.id,
             src_slot = src_val.slot,
-            dst_id   = dst_val.id,
+            dst      = dst_val.id,
             dst_slot = dst_val.slot,
-            count    = cnt
+            count    = src_val.item.count
         }
         table.insert(plans, plan)
-        if src_cnt > 0 then
-            db:add_item(src_val.id, src_val.slot,
-                { name = src_val.item.name,
-                 count = src_cnt }
-            )
-        else
-            db:del_item(src_val.id, src_val.slot)
-            src_it:next()
-        end
-        
         db:add_item(dst_val.id, dst_val.slot,
-            { name = dst_val.item.name,
-             count = dst_cnt }
+            {
+                name  = src_val.item.name,
+                count = src_val.item.count
+            }
         )
-        if dst_cnt == stack then
-            dst_it:next()
-        end
+        db:del_item(src_val.id, src_val.slot)
+        src_it:next()
+        dst_it:next()
     end
     return plans
 end
 
-function move_planner.plan(srcs, dsts)
+-- Create plans for topping up slots
+-- where there are items whose counts
+-- are less than their stack sizes.
+function move_planner.top_up
+(db, stacks, src_id, dst_id)
     local plans = {}
-    print("INPUTS:")
-    print_inv(srcs)
-    print("OUTPUTS:")
-    print_inv(dsts)
+    local src_pred = function(curr)
+        local item = curr.item
+        return item ~= nil and curr.id == src_id
+    end
+    local src_it = fiter:new(db, src_pred)
+    src_it:first()
+    while not src_it:is_done() do
+        local src_val  = src_it:get()
+        local src_item = src_val.item
+        local stack    = stacks:get(src_item.name)
+        local dst_pred = function(curr)
+            local item = curr.item
+            return item ~= nil
+                and curr.id == dst_id
+                and item.name == src_item.name
+                and item.count < stack
+        end
+        local dst_it = fiter:new(db, dst_pred)
+        dst_it:first()
+        while not dst_it:is_done() and
+            src_item.count > 0 do
+            local dst_val  = dst_it:get()
+            local dst_item = dst_val.item
+            local cap = stack - dst_item.count
+            local cnt = math.min(
+                cap, src_item.count
+            )
+            local src_cnt = src_item.count - cnt
+            local dst_cnt = dst_item.count + cnt
+            local plan = {
+                src      = src_val.id,
+                src_slot = src_val.slot,
+                dst      = dst_val.id,
+                dst_slot = dst_val.slot,
+                count    = cnt
+            }
+            table.insert(plans, plan)
+            if src_cnt == 0 then
+                db:del_item(
+                    src_val.id, src_val.slot
+                )
+                src_item.count = 0
+            else
+                db:add_item(
+                    src_val.id, src_val.slot,
+                    {
+                        name  = src_item.name,
+                        count = src_cnt
+                    }
+                )
+                src_item.count = src_cnt
+            end
+            db:add_item(dst_val.id, dst_val.slot,
+                {
+                    name  = dst_item.name,
+                    count = dst_cnt
+                }
+            )
+            if dst_cnt == stack then
+                dst_it:next()
+            end
+        end
+        src_it:next()
+    end
     return plans
 end
 
